52ac01a1612d528de286840dd1fda8df54448234
Jess Lodge <jessicaklodge@gmail.com>
2019-08-05 22:41:00 +0100

    add deploy script

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/jessicalodge/development/jklodge.github.io/node_modules/gatsby-plugin-material-ui/gatsby-browser.js'),
+      options: {"plugins":[],"stylesProvider":{"injectFirst":true}},
+    },{
+      plugin: require('/Users/jessicalodge/development/jklodge.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/jessicalodge/development/jklodge.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/jessicalodge/development/jklodge.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("/Users/jessicalodge/development/jklodge.github.io/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-archive-js": () => import("/Users/jessicalodge/development/jklodge.github.io/src/pages/archive.js" /* webpackChunkName: "component---src-pages-archive-js" */),
+  "component---src-pages-index-js": () => import("/Users/jessicalodge/development/jklodge.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
