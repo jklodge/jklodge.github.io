ac5333ba0d6de532cb4bb2a56e4fffa6f4fea6f7
Jess Lodge <jessicaklodge@gmail.com>
2019-12-06 16:15:51 +0000

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/jessicalodge/development/jklodge.github.io/node_modules/gatsby-plugin-material-ui/gatsby-browser.js'),
-      options: {"plugins":[],"stylesProvider":{"injectFirst":true}},
-    },{
-      plugin: require('/Users/jessicalodge/development/jklodge.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
